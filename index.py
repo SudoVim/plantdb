05f882b6f37c86613ae3f3aed6556a719cc2d54f
Michael Renken <michaelarenken@gmail.com>
2019-03-17 16:48:00 -0500

    Added script for indexing the plantdb mongo database

---
+++ index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+index database(s) for plant data
+"""
+
+import sys
+import argparse
+
+import pymongo
+
+def main(argv):
+    """ main function """
+    parser = argparse.ArgumentParser(
+        description="index database(s) for plant data",
+    )
+    args = parser.parse_args(argv)
+
+    client = pymongo.MongoClient()
+    client.plantdb.plants.create_index(
+        [
+            ("name", pymongo.ASCENDING),
+        ],
+        unique=True,
+    )
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
